7e1090bfdd8ba26099f7db5c9b7b16e9a93280e3
bonteves43@gmail.com <bonteves43@gmail.com>
2025-06-25 14:03:07 +0800

    firstcommit

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+const Card = ({title}) => {
+
+const [hasLiked, setHasLiked] = useState(false);  
+  return (
+    <div className="card" >
+      <h2>{title}</h2>  
+      <button onClick={() => setHasLiked(!hasLiked) 
+      }>
+        {hasLiked ? 'Liked' : 'Like'}
+      </button>
+    </div>
+  )
+}
+
+useEffect(()=>{}, [])
+
+const App = () => {
+  const [hasLiked, setHasLiked] = useState(false);
+
+  return (
+    <div className='card-container'>
+      
+      <Card title="ONE" rating={true} isCool = {true}/>
+      <Card title="TWO"/>
+      <Card title="THREE"/>
+      
+      
+      
+    </div>
+    
+  )
+}
+
+export default App
