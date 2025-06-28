4107c14b78492763c2c83b8f32a38a3c616070f7
BonTeves43 <bonteves43@gmail.com>
2025-06-28 23:28:55 +0800

    update search -> Search

---
+++ src/components/Search.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+
+const Search = () => {
+    return (
+        <div className="search">HUHU
+            {/* <div>
+                <img src="./pics/search.svg" alt="" />
+            </div> */}
+        </div>
+    )
+}
+
+export default Search
